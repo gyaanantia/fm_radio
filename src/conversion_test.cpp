46274396453da12766cd2e415785ab5068d34902
Cam Brown <cambrown@u.northwestern.edu>
2023-03-10 00:32:23 -0600

    added conversion tester v2

--- src/conversion_test.cpp
+++ src/conversion_test.cpp
@@ -27,56 +27,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    // static unsigned char IQ[SAMPLES*4];
-    // static int left_audio[AUDIO_SAMPLES];
-    // static int right_audio[AUDIO_SAMPLES];
-
-    // if ( argc < 2 )
-    // {
-    //     printf("Missing input file.\n");
-    //     return -1;
-    // }
-    // /*
-    // // initialize the audio output
-    // int audio_fd = audio_init( AUDIO_RATE );
-    // if ( audio_fd < 0 )
-    // {
-    //     printf("Failed to initialize audio!\n");
-    //     return -1;
-    // }
-    // */
-    // int audio_fd = open("audio_dump.bin", O_WRONLY);
-
-    // FILE * usrp_file = fopen(argv[1], "rb");
-    // if ( usrp_file == NULL )
-    // {
-    //     printf("Unable to open file.\n");
-    //     return -1;
-    // }    
-    
-    // // run the FM receiver 
-    // while( !feof(usrp_file) )
-    // {
-    //     // get I/Q from data file
-    //     fread( IQ, sizeof(char), SAMPLES*4, usrp_file );
-
-    //     // fm radio in mono
-    //     fm_radio_stereo( IQ, left_audio, right_audio );
-
-    //     // write to audio output
-    //     audio_tx( audio_fd, AUDIO_RATE, left_audio, right_audio, AUDIO_SAMPLES );
-    // }
-    int pi_fixed = 1 * QUANTIZE_F(PI);
-
-    // fclose( usrp_file );
-    // close( audio_fd );
-    printf("QUANT_VAL = %x\n", QUANT_VAL);
-    printf("(PI) = %08x\n", (float)(3.1415926535897932384626433832795f));
-    printf("(float)(PI) * (float)QUANT_VAL = %08x\n", ((float)(3.1415926535897932384626433832795f) * (float)QUANT_VAL));
-    printf("pi_fixed = %08x\n", pi_fixed);
-    printf("(MAX_DEV) = %08x\n", (float)(MAX_DEV));
-    printf("(TAU) = %08x\n", (float)(TAU));
-    printf("(W_PP) = %08x\n", (float)(W_PP));
+    // printf yo shit here
     printf("QUANTIZE_F(PI) = %08x\n", QUANTIZE_F(PI));
     printf("QUANTIZE_F(MAX_DEV) = %08x\n", QUANTIZE_F(MAX_DEV));
     printf("QUANTIZE_F(TAU) = %08x\n", QUANTIZE_F(TAU));
