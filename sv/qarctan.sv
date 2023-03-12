2f74900f28f908f178fca01d554cf0253809ae93
Cam Brown <cambrown@u.northwestern.edu>
2023-03-11 22:53:55 -0600

    demodulate started

--- sv/qarctan.sv
+++ sv/qarctan.sv
@@ -1,7 +1,3 @@
-`include "coeffs.svh"
-// import macros::*;
-import coeffs::*;
-
 module qarctan 
 (
     input   logic           clk,
@@ -25,8 +21,9 @@ logic [31:0] x_minus_abs_y;
 logic [31:0] x_plus_abs_y;
 logic [31:0] abs_y_minus_x;
 logic [31:0] quant_x_minus_abs_y;
-logic [31:0] dequant_quad_one_times_r;
 logic [31:0] quant_x_plus_abs_y;
+logic [63:0] quad_one_times_r;
+logic [31:0] lower_quad_one_times_r;
 
 // divider instance signals
 logic [63:0] dividend;
@@ -36,40 +33,43 @@ logic [31:0] remainder;
 logic overflow;
 logic div_valid_out;
 
-divider divider_inst (
-    .clk(clk),
-    .reset(reset),
-    .start(start),
-    .dividend(dividend),
-    .divisor(divisor),
-    .quotient(quotient),
-    .remainder(remainder),
-    .overflow(overflow)
-    .valid_out(div_valid_out)
-);
+// divider divider_inst (
+//     .clk(clk),
+//     .reset(reset),
+//     .start(start),
+//     .dividend(dividend),
+//     .divisor(divisor),
+//     .quotient(quotient),
+//     .remainder(remainder),
+//     .overflow(overflow),
+//     .valid_out(div_valid_out)
+// );
 
 assign abs_y = (($signed(y) >= 0) ? y : (~y + 32'h00000001)) + 32'h00000001;
-assign x_minux_abs_y = $signed(x) - $signed(abs_y);
+assign x_minus_abs_y = $signed(x) - $signed(abs_y);
 assign x_plus_abs_y = $signed(x) + $signed(abs_y);
 assign abs_y_minus_x = $signed(abs_y) - $signed(x);
 assign quant_x_minus_abs_y = x_minus_abs_y << 10;
 assign quant_x_plus_abs_y = x_plus_abs_y << 10;
+assign quad_one_times_r = $signed(QUAD_ONE) * $signed(r);
+assign lower_quad_one_times_r = quad_one_times_r;
+
 
 always_comb begin
     if ($signed(x) >= 0) begin
         // sign extend the dividend
         dividend = ($signed(quant_x_minus_abs_y) >= 0) ? {32'h0, quant_x_minus_abs_y} : {32'hffffffff, quant_x_minus_abs_y};
         divisor = x_plus_abs_y;
-        r = quotient[31:0];
+        r = dividend / divisor;
         // angle = quad1 - DEQUANTIZE(quad1 * r)
-        angle = $signed(QUAD_ONE) - $signed($signed((QUAD_ONE * r)[31:0]) >>> 10);
+        angle = $signed(QUAD_ONE) - $signed($signed(lower_quad_one_times_r) >>> 10);
     end
     else begin
         dividend = ($signed(quant_x_plus_abs_y) >= 0) ? {32'h0, quant_x_plus_abs_y} : {32'hffffffff, quant_x_plus_abs_y};
         divisor = abs_y_minus_x;
-        r = quotient[31:0];
+        r = dividend / divisor;
         // angle = quad3 - DEQUANTIZE(quad1 * r)
-        angle = $signed(QUAD_THREE) - $signed($signed((QUAD_ONE * r)[31:0]) >>> 10);
+        angle = $signed(QUAD_THREE) - $signed($signed(lower_quad_one_times_r) >>> 10);
     end
 
     data_out = ($signed(y) < 0) ? (~angle + 32'h00000001) : angle;
