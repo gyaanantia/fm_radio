cb5ac7b4c65be980608ecf52e65b9da32a92183c
Cam Brown <cambrown@u.northwestern.edu>
2023-03-13 22:28:03 -0500

    wee edits to conv test

--- src/conversion_test.cpp
+++ src/conversion_test.cpp
@@ -28,11 +28,15 @@ using namespace std;
 int main(int argc, char **argv)
 {
     // printf yo shit here
-    printf("QUANTIZE_F(PI) = %08x\n", QUANTIZE_F(PI));
-    printf("QUANTIZE_F(MAX_DEV) = %08x\n", QUANTIZE_F(MAX_DEV));
-    printf("QUANTIZE_F(TAU) = %08x\n", QUANTIZE_F(TAU));
-    printf("QUANTIZE_F(W_PP) = %08x\n", QUANTIZE_F(W_PP));
-    printf("");
+    // printf("QUANTIZE_F(PI) = %08x\n", QUANTIZE_F(PI));
+    // printf("QUANTIZE_F(MAX_DEV) = %08x\n", QUANTIZE_F(MAX_DEV));
+    // printf("QUANTIZE_F(TAU) = %08x\n", QUANTIZE_F(TAU));
+    // printf("QUANTIZE_F(W_PP) = %08x\n", QUANTIZE_F(W_PP));
+    printf("QUANTIZE_F(PI / 4.0) = %08x\n", QUANTIZE_F(PI / 4.0));
+    printf("QUANTIZE_F(3.0 * PI / 4.0) = %08x\n", QUANTIZE_F(3.0 * PI / 4.0));
+    printf("DEQUANTIZE(ffffc392) = %08x\n", DEQUANTIZE(0xffffc392));
+    printf("FM_DEMOD_GAIN = %08x\n", QUANTIZE_F( (float)QUAD_RATE / (2.0f * PI * MAX_DEV) ));
+    
 
     return 0;
 }
